f0a810d6ed6ce2da9aab7c2bcba2adc2df790d66
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2017-04-14 13:35:08 -0400

    implement user configuration

--- pytattle/reporters/__init__.py
+++ pytattle/reporters/__init__.py
@@ -1,11 +1,21 @@
 class Reporter(object):
     """Base class for crash reporters.
+
+    Args:
+        config: A :class:`Config` object. The options specific to this reporter
+            are extracted and stored in :attr:`self.config`.
+        kwargs: Any additional config options passed at runtime. These override
+            any values in `config`.
     """
     name = 'undefined'
     defaults = {}
-    
-    def __init__(self, **kwargs):
+    required = {}
+
+    def __init__(self, config, **kwargs):
         self.config = self.defaults.copy()
+        if config.has_section(self.name):
+            for option in config.options(self.name):
+                self.config[option] = config.get(self.name, option)
         self.config.update(kwargs)
     
     def __getattr__(self, name):
@@ -19,6 +29,29 @@ class Reporter(object):
         Args:
             user: The user to configure.
         """
+        if not user.has_section(self.name):
+            user.add_section(self.name)
+        for option in self.required:
+            if not user.has_option(self.name, option):
+                if option in self.config:
+                    value = self.config[option]
+                else:
+                    value = self.ask_for(option)
+                # type conversion
+                value = self.required[option](value)
+                user.set(self.name, option, value)
+    
+    def ask_for(self, option):
+        """Ask the user to enter a configuration value.
+
+        Args:
+            option: The option name.
+            obscure: Whether to obscure the input (e.g. for passwords).
+        """
+        #ask(
+        #    "Please enter a value for reporting method {} option {}".format(
+        #        self.name, option), 
+        #    obscure=obscure)
         pass
 
     def check_previous(self, error, user=None):
